caba2730120d40f4c8bfd21530c90a8f44f4bc5e
Surface <surface@gmail.com>
2025-03-07 02:42:21 -0800

    Added CredicCard class and SecureCreditCard class an inheritance of CreditCard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+
 df = pd.read_csv('hotels.csv', dtype={"id":str})
+card_df = pd.read_csv('cards.csv', dtype=str).to_dict(orient="records")
+secure_creditCard_df = pd.read_csv('card_security.csv', dtype=str)
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -32,14 +35,44 @@ class ReservationTicket:
 """
         return content
 
+class CreditCard:
+    def __init__(self, card_number):
+        self.number = card_number
+
+    def validate(self, expiration, cvc, holder):
+        card_data = {'number': self.number,
+                     'expiration': expiration,
+                     'cvc': cvc, 'holder': holder}
+        if card_data in card_df:
+            return True
+        else:
+            return False
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self, password):
+        if secure_creditCard_df[secure_creditCard_df["number"] == self.number]['password'].squeeze() == password:
+            return True
+        else:
+            return False
+
+
+
+
 print(df)
 
 hotel_id = input("Enter id of the hotel you want: ")
 hotel = Hotel(hotel_id)
+card = SecureCreditCard("1234")
 if hotel.available():
-    customer_name = input("Enter your name: ")
-    hotelReservation = ReservationTicket(customer_name, hotel)
-    print(hotelReservation.generate())
-    hotel.book()
+    if card.validate("12/26","123","JOHN SMITH"):
+        if card.authenticate("mypass"):
+            customer_name = input("Enter your name: ")
+            hotelReservation = ReservationTicket(customer_name, hotel)
+            hotel.book()
+            print(hotelReservation.generate())
+        else:
+            print("Credit card authentication failed!")
+    else:
+        print("There was a problem while processing your payment")
 else:
     print('The hotel is fully booked')
